68f6c97676d6c6cb6153ec4a9ae4649362802557
oaattia <oaattia@gmail.com>
2020-01-29 00:18:43 +0100

    use tasks for automate the bundling

--- src/tasks/build.js
+++
@@ -1,2 +0,0 @@
-// require some modules so you can compile the files from the typescript
-// and have the same file to be imported here
---
+++ src/tasks/copy.js
@@ -0,0 +1,27 @@
+const fs = require('fs-extra');
+const globby = require('globby');
+
+const baseDir = 'src';
+let descDir = 'build';
+
+const paths = [
+    'icon.png',
+    'manifest.json',
+].map((path) => `${baseDir}/${path}`);
+
+async function copy() {
+    const files = await globby(paths);
+    console.log(files);
+    const destDir = `${descDir}`;
+
+    for (const file of files) {
+        if (!file.startsWith(`${baseDir}/`)) {
+            throw new Error(`Unable to handle path "${file}"`);
+        }
+        const dest = `${destDir}/${file.substring(baseDir.length + 1)}`;
+        console.log(file, dest);
+        await fs.copy(file, dest);
+    }
+}
+
+copy();
